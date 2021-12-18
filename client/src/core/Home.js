d3334b0b6463945ebc694d708d6a3fd19e84b99c
aniket1104 <aniketkumar2001@gmail.com>
2021-12-18 23:27:37 +0530

    =home-page

---
+++ client/src/core/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header"
+
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+export default Home
