875a23b3d1ef9de6d4d84d2fe856fa2feecce056
kumar <harshme78@gmail.com>
2022-01-02 21:44:48 +0530

    mongo

--- server/index.js
+++ server/index.js
@@ -1,4 +1,11 @@
 import  Express  from "express";
+import Connection from "./database/db.js";
+
+
 
 const app = Express();
 console.log("hello");
+const PORT  = 8000;
+app.listen(PORT,console.log("server running"));
+
+Connection();
